f34d43d9106f3acbbd02729e5bfd3b196d6fcd00
enneiymyhamza06-wq <enneiymyhamza06@gmail.com>
2026-01-05 01:02:21 +0100

    edit function

--- js/app.js
+++ js/app.js
@@ -1,141 +1,155 @@
 
 const users = [];
 let action = "";
-
+// mahd dakchi li wst while (true) radi tb9a while khdama
 while (true) {
-  action = prompt("What do you want to do? 'signup', 'login', 'change password', or 'exit' ");
-// 1
-  if (action === 'exit') {
-    alert("bye");
-    break;
-  }
-
-  if (action === 'signup' || action === 'login' || action === 'change password') {
-    break;
-  } else {
-    alert("dkhl chi haja s7i7a");
-  }
-}
-// 2
-if (action === 'signup') {
-  let name;
-  while (true) {
-    name = prompt("dkhl fullName").trim();
-    if (name.length < 5) { 
-alert("name khaso ykoun kbir mn 5 caracter");
- continue; }
-    if (!/^[a-zA-Z ]+$/.test(name)){
-             alert("name ma fihch'a-zA-Z'")
-             continue;
-             }
-    name = name.toLowerCase().replace(/(^|\s)\S/g, match => match.toUpperCase());
-    break;
-  }
-
-  let email;
-  while (true) {
-    email = prompt("dkhl email").trim().toLowerCase();
-    if (email.includes(" ")) { 
-        alert("email ma fihch ipace")
-         continue;
-     }
-    if (email.length < 10) {
-         alert("email khaso ykoun >= 10 caractères")
-
-         continue;
-         }
-    if (!email.includes("@") || email.indexOf("@") !== email.lastIndexOf("@")) { 
-        alert("email khaso ykoun fih ghir @ wahed")
-        continue; 
+    // 1
+    action = prompt("What do you want to do? 'signup', 'login', 'change password', or 'exit' ");
+    if (action === "exit") break;
+    else if (action === "signup") {
+        signup();
     }
-    if (users.find(u => u.email === email)) {
-         alert("email deja kayn")
-         continue 
-        }
-    break;
-  }
-let age;
-while (true) {
-      age = prompt ("ch7al f 3omrk").trim().Number;
-      if (age == Number) {
-        continue;
-      }
-      if (age != 0 || age > 3) {
-        alert("ach dylk howa: " + age)
-        break
-      }
-     
+    else if (action === "login") {
+        login();
+    }
+    else if (action === "change password") {
+        change_password();
+    }
+    else {
+        alert("dkhl chi haja s7i7a");
+    }
 }
 
-  let password;
-  while (true) {
-    password = prompt("dkhl password");
-    if (password.includes(" ")){ 
-alert("Password ma fihch space")
-continue;
- }
-    if (password.length < 7) { 
-        alert("Password khaso >= 7 lettres")
-        continue;
-     }
-    if (!/[@#\-\+\*\/]/.test(password)) {
-         alert("Password khaso ykoun fih character mn hado: [@, #, -, +, *, /].")
-         continue; 
-         break;
-         }
-    let I3ADApASS = prompt("3awd dkhl password ola ghadi tbloka")
- if (I3ADApASS === password) {
-    alert("nadiii")
- }else{
-    break
- }
-  }
-  users.push({ name, 
-    email, 
-    password,
-    age,
-   balance:0,
-    history:[]  
-}
-)
-
-  alert("signup nadiii");
-  console.log(users);
-}
-action = prompt("What do you want to do? 'signup', 'login', 'change password', or 'exit' ");
-// 
-if (action == 'login') {
-    let emaill ;
-    let passwordd;
+// 2
+// ila ktb signup
+function signup() {
+    let name;
+    //   mahd dakchi li wst while true radi tb9 khdama 
     while (true) {
-        emaill = prompt("dkhl email dylk")
-        if (email === emaill) {
-            alert("bhal bhal")
+        // trim:kathayd espace li jnab
+        name = prompt("dkhl fullName");
+        // 
+        if (name.length < 5) {
+            alert("name khaso ykoun kbir mn 5 caracter");
             continue;
         }
-        if (passwordd === password) {
-            alert("hta passwordat bjoj bhal bhal")
+        // ILA MAKANCH FIH horof radi maradich ykml / test:katisti lk dakchi li kayn ldakhl li wst name
+        if (!/^[a-zA-Z ]+$/.test(name)) {
+            alert("name ma fihch'a-zA-Z'")
+            continue;
         }
+        // kayrj3 ga3 lhorof sghar o kayrj3 awl harf kbir 
+        name = name.toLowerCase().replace(/(^|\s)\S/g, match => match.toUpperCase());
+        break;
     }
-    let changePass = prompt("ghiti tbdl password");
+
+    let email;
+    while (true) {
+        email = prompt("dkhl email").trim().toLowerCase();
+        if (email.includes(" ")) {
+            alert("email ma fihch ipace")
+            continue;
+        }
+        if (email.length < 10) {
+            alert("email khaso ykoun >= 10 caractères")
+            continue;
+        }
+
+
+        if (!email.includes("@") || email.indexOf("@") !== email.lastIndexOf("@")) {
+            alert("email khaso ykoun fih ghir @ wahed")
+            continue;
+        }
+        if (users.find(u => u.email === email)) {
+            alert("email deja kayn")
+            continue
+        }
+        break;
+    }
+    let age;
+    while (true) {
+        age = Number(prompt("ch7al f 3omrk"));
+        if (isNaN(age) || age < 3) {
+            alert("dkhl age s7i7");
+            continue;
+        }
+        break;
+    }
+
+    let password;
+    while (true) {
+        password = prompt("dkhl password");
+        if (password.includes(" ")) {
+            alert("Password ma fihch space")
+            continue;
+        }
+        if (password.length < 7) {
+            alert("Password khaso >= 7 lettres")
+            continue;
+        }
+        if (!/[@#\-\+\*\/]/.test(password)) {
+            alert("Password khaso ykoun fih character mn hado: [@, #, -, +, *, /].")
+            continue;
+            break;
+        }
+        let I3ADApASS = prompt("3awd dkhl password ola ghadi tbloka")
+        if (I3ADApASS !== password) {
+            alert("password machi bhal bhal");
+            continue;
+        }
+        break;
+    }
+    users.push({
+        name,
+        email,
+        password,
+        age,
+        balance: 0,
+        history: []
+    }
+    )
+
+    alert("signup nadiii");
+    console.log(users);
+
 }
+
+// action = prompt("What do you want to do? 'signup', 'login', 'change password', or 'exit' ");
+//
+function login() {
+    let email = prompt("dkhl email dylk");
+    let password = prompt("dkhl password dylk");
+
+    let user = users.find(
+        u => u.email === email && u.password === password);
+
+    if (!user) {
+        alert("email ola password ghalt");
+        return;
+    }
+
+    alert("welcome " + user.name);
+}
+
+
 // 3
- if (action === "change password") {
-        let email = prompt("ktb email dylk:");
+function change_password() {
 
-        let user = users.find(dswwsdw => dswwsdw.email === email);
+    let email = prompt("ktb email dylk:");
 
-        if (! user)
-            {
-            alert("email dylk machi shih");
-        }
-        else
-        {
-            let newPassword = prompt("ktb password dylk:");
-            user.password = newPassword;
-            alert("Password li bdlti daz");
-        }
+    let user = users.find(dswwsdw => dswwsdw.email === email);
+
+    if (!user) {
+        alert("email dylk machi shih");
     }
-    else{
-        alert("3awd mra khra");
+    else {
+        let newPassword = prompt("ktb password dylk:");
+        user.password = newPassword;
+        alert("Password li bdlti daz");
     }
+    
+}
+// else{
+//     alert("3awd mra khra");
+// }
+
